55e8208e8ddb0f9c9bb0e8be6fbe5f0f1219f4b4
Subhasis Debsharma (IN4424) <M1044424@mindtree.com>
2018-09-23 20:06:50 +0530

    1.change+subhasis

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ exports.handler = (event, context, callback) => {
     currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
         statusCode: '200',
-        body: 'The time <<Test:Subhasis>>: ' + currentTime.toString(),
+        body: 'The time <<1.change:Subhasis>>: ' + currentTime.toString(),
     });
 };
