b6ae70a5110de7f10a22c303d70ba2ef77996fb0
Gursimran Singh <GSingh@mindtree_odc01.com>
2018-09-25 18:09:30 +0530

    adding cors headers

--- src/lambdas/products/getProduct.js
+++ src/lambdas/products/getProduct.js
@@ -1,30 +1,35 @@
 const productModel = require('../../models/productsModel');
 
 exports.handler = (event, context, callback) => {
-    
+
     const id = event.pathParameters.id;
 
     productModel.getProduct(id)
-         .then (product => {
+        .then(product => {
             var response = {
                 "statusCode": 200,
                 "headers": {
-                    "content-type": "application/json"
+                    "content-type": "application/json",
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': true,
+
                 },
                 "body": JSON.stringify(product),
                 "isBase64Encoded": false
             };
             callback(null, response);
-         })
-         .catch(err => {
+        })
+        .catch(err => {
             var response = {
                 "statusCode": 500,
                 "headers": {
-                    "content-type": "application/json"
+                    "content-type": "application/json",
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': true,
                 },
                 "body": JSON.stringify(err),
                 "isBase64Encoded": false
             };
             callback(null, response);
-         })
+        })
 };
--- src/lambdas/products/getProducts.js
+++ src/lambdas/products/getProducts.js
@@ -4,20 +4,28 @@ exports.handler = (event, context, callback) => {
     console.log('getProducts handler called')
 
     productsModel.getProducts()
-         .then (products => {
+        .then(products => {
             var response = {
                 "statusCode": 200,
+                "headers": {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': true,
+                },
                 "body": JSON.stringify(products),
                 "isBase64Encoded": false
             };
             callback(null, response);
-         })
-         .catch(err => {
+        })
+        .catch(err => {
             var response = {
                 "statusCode": 500,
+                "headers": {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': true,
+                },
                 "body": JSON.stringify(err),
                 "isBase64Encoded": false
             };
             callback(null, response);
-         })
+        })
 };
