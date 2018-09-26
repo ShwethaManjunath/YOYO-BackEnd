847741f3167dec123ab7843a4aadde51877ace0e
Subhasis Debsharma (IN4424) <M1044424@mindtree.com>
2018-09-26 18:35:38 +0530

    loginUser

--- src/lambdas/users/loginUser.js
+++ src/lambdas/users/loginUser.js
@@ -11,9 +11,7 @@ admin.initializeApp({
 
 // Login Handler
 exports.handler = (event, context, callback) => {
-    
     const eventBody = JSON.parse(event.body);
-
     admin.auth().verifyIdToken(eventBody.firebaseIdToken)
     .then(user => {
         // console.log('User>>: ', user)
