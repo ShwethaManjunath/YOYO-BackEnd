b084918c51a7416edfd1707bae17a631773976c3
Gursimran Singh <GSingh@mindtree_odc01.com>
2018-09-26 11:37:20 +0530

    changing products lambda

--- src/lambdas/products/getProducts.js
+++ src/lambdas/products/getProducts.js
@@ -2,8 +2,44 @@ const productsModel = require('../../models/productsModel');
 
 exports.handler = (event, context, callback) => {
     console.log('getProducts handler called')
+    
+    console.log(event.queryStringParameters);
 
-    productsModel.getProducts()
+    if(event.queryStringParameters !== null) {
+        const id = event.queryStringParameters.id;
+        const category_id = event.queryStringParameters.categoryId;
+        console.log('inside query params');
+        productsModel.getProduct(id , category_id)
+        .then(product => {
+            var response = {
+                "statusCode": 200,
+                "headers": {
+                    "content-type": "application/json",
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': true,
+
+                },
+                "body": JSON.stringify(product),
+                "isBase64Encoded": false
+            };
+            callback(null, response);
+        })
+        .catch(err => {
+            var response = {
+                "statusCode": 500,
+                "headers": {
+                    "content-type": "application/json",
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': true,
+                },
+                "body": JSON.stringify(err),
+                "isBase64Encoded": false
+            };
+            callback(null, response);
+        })
+    } else {
+        console.log('inside all products');
+        productsModel.getProducts()
         .then(products => {
             var response = {
                 "statusCode": 200,
@@ -27,5 +63,6 @@ exports.handler = (event, context, callback) => {
                 "isBase64Encoded": false
             };
             callback(null, response);
-        })
+        });
+    }
 };
