612cbf86191315351495d967656176dfd268bbb3
Aman Aditya <M1042937@mindtree.com>
2018-09-27 12:50:22 +0530

    getTransactions Lambda Changed for getById

--- src/lambdas/transaction/getTransactions.js
+++ src/lambdas/transaction/getTransactions.js
@@ -4,8 +4,40 @@ exports.handler = (event, context, callback) => {
     var params = {
         TableName : "Transactions",
     };
+    if(event.queryStringParameters) {
+        const id = event.queryStringParameters.id;
+        TransactionModel.getTransaction(id)
+         .then (transaction => {
+            var response = {
+                "statusCode": 200,
+                "headers": {
+                    "content-type": "application/json",
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': true,
+                },
+                "body": JSON.stringify(transaction),
+                "isBase64Encoded": false
+            };
+            callback(null, response);
+         })
+         .catch(err => {
+            var response = {
+                "statusCode": 500,
+                "headers": {
+                    "content-type": "application/json",
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': true,
+                },
+                "body": JSON.stringify(err),
+                "isBase64Encoded": false
+            };
+            callback(null, response);
+         })
+    } 
+        
+    
 
-    TransactionModel.getTransactions(params)
+   else{ TransactionModel.getTransactions(params)
          .then (transaction => {
             var response = {
                 "statusCode": 200,
@@ -28,4 +60,5 @@ exports.handler = (event, context, callback) => {
             };
             callback(null, response);
          })
+   }
 };
