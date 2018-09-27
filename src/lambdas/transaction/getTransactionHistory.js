e0ee372ee1c7dc18c2acc7ff8651c8bd7b54b769
Shwetha M <M1045570@mindtree.com>
2018-09-27 08:52:20 +0530

    modified transaction history

--- src/lambdas/transaction/getTransactionHistory.js
+++ src/lambdas/transaction/getTransactionHistory.js
@@ -1,15 +1,17 @@
 const TransactionModel = require('../../models/transactionModel');
 
 exports.handler = (event, context, callback) => {
-  const id = parseInt(event.pathParameters.sender_id);
+  //const id = parseInt(event.pathParameters.sender_id);
+  const id = event.queryStringParameters.sender_id;
 
   TransactionModel.getTransactionHistory(id)
     .then(transactions => {
       var response = {
         "statusCode": 200,
         "headers": {
+          "content-type": "application/json",
           'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Credentials': true
+          'Access-Control-Allow-Credentials': true,
         },
         "body": JSON.stringify(transactions),
         "isBase64Encoded": false
@@ -20,8 +22,9 @@ exports.handler = (event, context, callback) => {
       var response = {
         "statusCode": 500,
         "headers": {
+          "content-type": "application/json",
           'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Credentials': true
+          'Access-Control-Allow-Credentials': true,
         },
         "body": JSON.stringify(err),
         "isBase64Encoded": false
