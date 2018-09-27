6b4d1d474ef5643b67b3b4f1d22fd1b5cbe9aac2
Shwetha M <M1045570@mindtree.com>
2018-09-27 17:44:35 +0530

    modified get user method

--- src/lambdas/users/getUser.js
+++ src/lambdas/users/getUser.js
@@ -2,34 +2,65 @@ const userModel = require('../../models/userModel');
 
 exports.handler = (event, context, callback) => {
 
-    const email = event.pathParameters.email;
+    if (event.queryStringParameters) {
+        const email = event.queryStringParameters.email;
 
-    userModel.getUser(email)
-        .then(user => {
-            var response = {
-                "statusCode": 200,
-                "headers": {
-                    "content-type": "application/json",
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Credentials': true,
-
-                },
-                "body": JSON.stringify(user),
-                "isBase64Encoded": false
-            };
-            callback(null, response);
-        })
-        .catch(err => {
-            var response = {
-                "statusCode": 500,
-                "headers": {
-                    "content-type": "application/json",
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Credentials': true,
-                },
-                "body": JSON.stringify(err),
-                "isBase64Encoded": false
-            };
-            callback(null, response);
-        })
+        userModel.getUser(email)
+            .then(user => {
+                var response = {
+                    "statusCode": 200,
+                    "headers": {
+                        "content-type": "application/json",
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Allow-Credentials': true,
+                    },
+                    "body": JSON.stringify(user),
+                    "isBase64Encoded": false
+                };
+                callback(null, response);
+            })
+            .catch(err => {
+                var response = {
+                    "statusCode": 500,
+                    "headers": {
+                        "content-type": "application/json",
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Allow-Credentials': true,
+                    },
+                    "body": JSON.stringify(err),
+                    "isBase64Encoded": false
+                };
+                callback(null, response);
+            })
+    }
+    else {
+        userModel.getUser(emai)
+            .then(user => {
+                var response = {
+                    "statusCode": 200,
+                    "headers": {
+                        "content-type": "application/json",
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Allow-Credentials': true,
+                    },
+                    "body": JSON.stringify(user),
+                    "isBase64Encoded": false
+                };
+                callback(null, response);
+            })
+            .catch(err => {
+                var response = {
+                    "statusCode": 500,
+                    "headers": {
+                        "content-type": "application/json",
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Allow-Credentials': true,
+                    },
+                    "body": JSON.stringify(err),
+                    "isBase64Encoded": false
+                };
+                callback(null, response);
+            })
+    }
 };
+
