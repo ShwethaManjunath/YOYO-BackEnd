63ff27d529aef6fb74c02ce93876351a62b0bdc0
Aman Aditya <M1042937@mindtree.com>
2018-09-27 15:15:07 +0530

    response body changed for transaction Lambda

--- src/lambdas/transaction/postTransaction.js
+++ src/lambdas/transaction/postTransaction.js
@@ -13,7 +13,7 @@ exports.handler = (event, context, callback) => {
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Credentials': true,
             },
-            "body": transaction,
+            "body": JSON.stringify(transaction),
             "isBase64Encoded": false
         };
         callback(null, response);
