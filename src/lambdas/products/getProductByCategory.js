68f1449b0f7508da9a849c986000ce9219194f5f
Chetan-at-599408351101 <chetan.hiregoudra@mindtree.com>
2018-09-28 12:50:43 +0530

    pushing sorting api

--- src/lambdas/products/getProductByCategory.js
+++ src/lambdas/products/getProductByCategory.js
@@ -1,8 +1,44 @@
 const productsModel = require('../../models/productsModel');
 
 exports.handler = (event, context, callback) => {
-    const id = event.queryStringParameters.categoryId;
-        productsModel.getProductByCategory(id)
+    var prods = [];
+  //  console.log(JSON.parse(event.queryStringParameters.category_ids).ids);
+    const ids = JSON.parse(event.queryStringParameters.category_ids).ids;
+    
+    if(ids.length > 1){
+        new Promise((resolve, reject) => {
+            for(let i=0;i<ids.length;i++){
+                console.log("check",ids[i]);
+                productsModel.getProductByCategory(ids[i])
+                .then(products => {
+                    console.log(products)
+                     prods.push(...products); 
+                     console.log(i,prods);
+                     if(i + 1 === ids.length)
+                        resolve();
+                })
+                .catch(err=>{
+                    console.log(err);
+                    
+                })
+            }
+        })
+        .then(result => {
+            console.log("prod",prods);
+            var response = {
+                "statusCode": 200,
+                "headers": {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': true,
+                },
+                "body": JSON.stringify(prods),
+                "isBase64Encoded": false
+            };
+            callback(null, response);    
+        })
+        
+    }else{
+        productsModel.getProductByCategory(ids[0])
         .then(products => {
             var response = {
                 "statusCode": 200,
@@ -27,4 +63,6 @@ exports.handler = (event, context, callback) => {
             };
             callback(null, response);
         });
+    }
+        
     }
